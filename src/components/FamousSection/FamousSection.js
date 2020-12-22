c6b9e74909786e71b2fdc9083b1552587c39a3ce
Casie Siekman <casiesiekman@Casies-MacBook-Pro.local>
2020-12-22 13:35:34 -0600

    update to spread operator

--- src/components/FamousSection/FamousSection.js
+++ src/components/FamousSection/FamousSection.js
@@ -2,26 +2,24 @@ import React, { useEffect, useState } from 'react';
 import './FamousSection.css';
 
 function FamousSection() {
-
-  let [famousPersonName, handleChangeForName] = useState("");
-  let [famousPersonRole, handleChangeForRole] = useState("");
+  let [famousPerson, setPerson] = useState({name:"", role:""});
 
   const addPerson = (evt) => {
     evt.preventDefault();
-    console.log( `The person is ${famousPersonName} and they're famous for ${famousPersonRole}` );
+    console.log( `The person is ${famousPerson.name} and they're famous for ${famousPerson.role}` );
   }
 
     return (
       <section className="new-person-section">
         <form onSubmit={addPerson}>
           <label htmlFor="name-input">Name:</label>
-          <input id="name-input" onChange={e => handleChangeForName(e.target.value)} />
+          <input id="name-input" onChange={e => setPerson({...famousPerson, name: e.target.value})} />
           <label htmlFor="role-input">Famous for:</label>
-          <input id="role-input" onChange={e => handleChangeForRole(e.target.value)} />
+          <input id="role-input" onChange={e => setPerson({...famousPerson, role: e.target.value})} />
           <button type="submit">Done</button>
         </form>
         <p>
-          {famousPersonName} is famous for "{famousPersonRole}".
+          {famousPerson.name} is famous for "{famousPerson.role}".
         </p>
         <ul>
           {/* The list should go here. */}
